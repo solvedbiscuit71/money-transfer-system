f8c33d67ed0868f4c2b20f6ca36891ba9d8e14a2
Praveen Perumal <ppraveen98841@gmail.com>
2026-06-22 08:45:43 +0530

    Increased page limit from 7 to 8

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'http://localhost:8080/api/v1',
-    pageLimit: 7,
+    pageLimit: 8
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     baseUrl: 'http://localhost:8080/api/v1',
-    pageLimit: 7,
+    pageLimit: 8
 };
